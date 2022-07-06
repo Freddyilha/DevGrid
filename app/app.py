94b910887267c55584e76f3a2bebd0929b9d8375
freddy <freddyilha@gmail.com>
2022-07-06 00:34:00 -0300

    CHORE: starting the post route

--- app/app.py
+++ app/app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from datetime import datetime
+import requests
+import responses
 
 app = Flask(__name__)
 
+API_KEY = "892ac4245af18b32aeb77969a8b3248c"
+
 @app.route("/")
-def hello():
+def home():
     return f"Hello world!"
+
+@app.route("/<int:uid>", methods=['GET'])
+def store_weather(uid):
+    now = datetime.now()
+    url = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API_KEY}"
+    return f"Hello world! {uid} - {now}"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
